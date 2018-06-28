c0edd32bcf3a661369ac52846e3b7fb836d71ecf
Geir Ulvik <ulvik@usit.uio.no>
2018-06-28 13:52:46 +0200

    migration for midlertidig test-database. Test av hosts-module og models.py med alle andre klasser commented ut.

--- mreg/api/models.py
+++ mreg/api/models.py
@@ -12,28 +12,28 @@
 from django.db import models
 
 
-class Zones(models.Model):
-    zoneid = models.AutoField(primary_key=True)
-    name = models.TextField(unique=True)
-    primary_ns = models.TextField()
-    email = models.TextField(blank=True, null=True)
-    serialno = models.IntegerField(blank=True, null=True)
-    refresh = models.IntegerField(blank=True, null=True)
-    retry = models.IntegerField(blank=True, null=True)
-    expire = models.IntegerField(blank=True, null=True)
-    ttl = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'zones'
-
-
-class Ns(models.Model):
-    zoneid = models.ForeignKey(Zones, models.DO_NOTHING, db_column='zoneid')
-    name = models.TextField()
-    ttl = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'ns'
+# class Zones(models.Model):
+#     zoneid = models.AutoField(primary_key=True)
+#     name = models.TextField(unique=True)
+#     primary_ns = models.TextField()
+#     email = models.TextField(blank=True, null=True)
+#     serialno = models.IntegerField(blank=True, null=True)
+#     refresh = models.IntegerField(blank=True, null=True)
+#     retry = models.IntegerField(blank=True, null=True)
+#     expire = models.IntegerField(blank=True, null=True)
+#     ttl = models.IntegerField(blank=True, null=True)
+#
+#     class Meta:
+#         db_table = 'zones'
+#
+#
+# class Ns(models.Model):
+#     zoneid = models.ForeignKey(Zones, models.DO_NOTHING, db_column='zoneid')
+#     name = models.TextField()
+#     ttl = models.IntegerField(blank=True, null=True)
+#
+#     class Meta:
+#         db_table = 'ns'
 
 
 class Hosts(models.Model):
@@ -50,86 +50,86 @@ class Hosts(models.Model):
     class Meta:
         db_table = 'hosts'
 
-
-class Subnets(models.Model):
-    range = models.TextField(blank=True, null=True)  # This field type is a guess.
-    comment = models.TextField(blank=True, null=True)
-    dns_delegated = models.NullBooleanField()
-
-    class Meta:
-        db_table = 'subnets'
-
-
-class ARecords(models.Model):
-    hostid = models.ForeignKey(Hosts, models.DO_NOTHING, db_column='hostid', blank=True, null=True)
-    name = models.TextField(blank=True, null=True)
-    ipaddress = models.GenericIPAddressField(blank=True, null=True)
-    ttl = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'a_records'
-
-
-class Aaaa(models.Model):
-    hostid = models.ForeignKey(Hosts, models.DO_NOTHING, db_column='hostid', blank=True, null=True)
-    name = models.TextField(blank=True, null=True)
-    ipaddress = models.GenericIPAddressField(blank=True, null=True)
-    ttl = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'aaaa'
-
-
-class Ptr(models.Model):
-    hostid = models.ForeignKey(Hosts, models.DO_NOTHING, db_column='hostid', blank=True, null=True)
-    ipaddress = models.GenericIPAddressField(blank=True, null=True)
-    name = models.TextField(blank=True, null=True)
-    ttl = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'ptr'
-
-
-class Cname(models.Model):
-    name = models.TextField(blank=True, null=True)
-    cname = models.TextField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'cname'
-
-
-class Naptr(models.Model):
-    naptrid = models.AutoField(primary_key=True)
-    preference = models.IntegerField(blank=True, null=True)
-    orderv = models.IntegerField(blank=True, null=True)
-    flag = models.CharField(max_length=1, blank=True, null=True)
-    service = models.TextField(blank=True, null=True)
-    regex = models.TextField(blank=True, null=True)
-    replacement = models.TextField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'naptr'
-
-
-class Srv(models.Model):
-    srvid = models.AutoField(primary_key=True)
-    service = models.TextField(blank=True, null=True)
-    proto = models.TextField(blank=True, null=True)
-    domain = models.TextField(blank=True, null=True)
-    priority = models.IntegerField(blank=True, null=True)
-    weight = models.IntegerField(blank=True, null=True)
-    port = models.IntegerField(blank=True, null=True)
-    ttl = models.IntegerField(blank=True, null=True)
-    target = models.TextField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'srv'
-
-
-class Txt(models.Model):
-    txtid = models.AutoField(primary_key=True)
-    hostid = models.ForeignKey(Hosts, models.DO_NOTHING, db_column='hostid')
-    txt = models.TextField()
-
-    class Meta:
-        db_table = 'txt'
+#
+# class Subnets(models.Model):
+#     range = models.TextField(blank=True, null=True)  # This field type is a guess.
+#     comment = models.TextField(blank=True, null=True)
+#     dns_delegated = models.NullBooleanField()
+#
+#     class Meta:
+#         db_table = 'subnets'
+#
+#
+# class ARecords(models.Model):
+#     hostid = models.ForeignKey(Hosts, models.DO_NOTHING, db_column='hostid', blank=True, null=True)
+#     name = models.TextField(blank=True, null=True)
+#     ipaddress = models.GenericIPAddressField(blank=True, null=True)
+#     ttl = models.IntegerField(blank=True, null=True)
+#
+#     class Meta:
+#         db_table = 'a_records'
+#
+#
+# class Aaaa(models.Model):
+#     hostid = models.ForeignKey(Hosts, models.DO_NOTHING, db_column='hostid', blank=True, null=True)
+#     name = models.TextField(blank=True, null=True)
+#     ipaddress = models.GenericIPAddressField(blank=True, null=True)
+#     ttl = models.IntegerField(blank=True, null=True)
+#
+#     class Meta:
+#         db_table = 'aaaa'
+#
+#
+# class Ptr(models.Model):
+#     hostid = models.ForeignKey(Hosts, models.DO_NOTHING, db_column='hostid', blank=True, null=True)
+#     ipaddress = models.GenericIPAddressField(blank=True, null=True)
+#     name = models.TextField(blank=True, null=True)
+#     ttl = models.IntegerField(blank=True, null=True)
+#
+#     class Meta:
+#         db_table = 'ptr'
+#
+#
+# class Cname(models.Model):
+#     name = models.TextField(blank=True, null=True)
+#     cname = models.TextField(blank=True, null=True)
+#
+#     class Meta:
+#         db_table = 'cname'
+#
+#
+# class Naptr(models.Model):
+#     naptrid = models.AutoField(primary_key=True)
+#     preference = models.IntegerField(blank=True, null=True)
+#     orderv = models.IntegerField(blank=True, null=True)
+#     flag = models.CharField(max_length=1, blank=True, null=True)
+#     service = models.TextField(blank=True, null=True)
+#     regex = models.TextField(blank=True, null=True)
+#     replacement = models.TextField(blank=True, null=True)
+#
+#     class Meta:
+#         db_table = 'naptr'
+#
+#
+# class Srv(models.Model):
+#     srvid = models.AutoField(primary_key=True)
+#     service = models.TextField(blank=True, null=True)
+#     proto = models.TextField(blank=True, null=True)
+#     domain = models.TextField(blank=True, null=True)
+#     priority = models.IntegerField(blank=True, null=True)
+#     weight = models.IntegerField(blank=True, null=True)
+#     port = models.IntegerField(blank=True, null=True)
+#     ttl = models.IntegerField(blank=True, null=True)
+#     target = models.TextField(blank=True, null=True)
+#
+#     class Meta:
+#         db_table = 'srv'
+#
+#
+# class Txt(models.Model):
+#     txtid = models.AutoField(primary_key=True)
+#     hostid = models.ForeignKey(Hosts, models.DO_NOTHING, db_column='hostid')
+#     txt = models.TextField()
+#
+#     class Meta:
+#         db_table = 'txt'
--- mreg/api/v1/tests.py
+++ mreg/api/v1/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from api.models import Hosts
+
+
+class ModelTestCase(TestCase):
+    """This class defines the test suite for the Hosts model."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.host_sample = Hosts(hostid=1,
+                                 name='dette-er-en-host',
+                                 ipaddress='127.0.0.1',
+                                 contact='ulvik@usit.uio.no')
+
+    def test_model_can_create_a_host(self):
+        """Test that the model is able to create a host."""
+        old_count = Hosts.objects.count()
+        self.host_sample.save()
+        new_count = Hosts.objects.count()
+        self.assertNotEqual(old_count, new_count)
