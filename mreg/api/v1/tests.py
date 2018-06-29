f01249137a4041c0750003abe845be9455b6b7df
Nils Hiorth <nils@nls.me>
2018-06-29 11:25:03 +0200

    Added tests for /hosts/

--- mreg/api/v1/tests.py
+++ mreg/api/v1/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from mreg.models import Hosts
+from rest_framework.test import APIClient
 
 
 class ModelTestCase(TestCase):
@@ -18,3 +19,33 @@ class ModelTestCase(TestCase):
         self.host_sample.save()
         new_count = Hosts.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+
+class APIHostsTestCase(TestCase):
+    """This class defines the test suite for api/hosts"""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.host_sample = Hosts(hostid=1,
+                                 name='dette-er-en-host',
+                                 ipaddress='127.0.0.1',
+                                 contact='ulvik@usit.uio.no')
+        self.patch_data = {'name': 'nytt-navn', 'contact': 'updated@mail.com'}
+
+    def test_hosts_patch_404_not_found(self):
+        """Patching a non-existing entry should return 404"""
+        self.host_sample.save()
+        client = APIClient()
+        response = client.patch('/hosts/feil-navn/', self.patch_data)
+        self.assertEqual(response.status_code, 404)
+
+    def test_hosts_patch_204_no_content(self):
+        """Patching an existing and valid entry should return 204"""
+        self.host_sample.save()
+        client = APIClient()
+        response = client.patch('/hosts/dette-er-en-host', self.patch_data)
+        self.assertEqual(response.status_code, 204)
+        self.assertContains(response['Location'], '/hosts/nytt-navn')
+
+
+
