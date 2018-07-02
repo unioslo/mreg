659d86416e3311046daf8989dfff077f215a2674
magnus <mhmorgan42@gmail.com>
2018-07-02 13:03:42 +0200

    class based REST API for host record

--- mreg/api/v1/views.py
+++ mreg/api/v1/views.py
@@ -1,19 +1,22 @@
+from django.http import Http404
+from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from mreg.models import Hosts
 from mreg.api.v1.serializers import HostsSerializer
 
 
-@api_view(['GET', 'POST'])
-def host_list(request, format=None):
-    """ List all hosts, or create a new host """
-    if request.method == 'GET':
+class HostList(APIView):
+    """
+    List all hosts, or create a new host.
+    """
+
+    def get(self, request, format=None):
         hosts = Hosts.objects.all()
         serializer = HostsSerializer(hosts, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
-        # data = JSONParser().parse(request)
+
+    def post(self, request, format=None):
         serializer = HostsSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -21,24 +24,31 @@ def host_list(request, format=None):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def host_detail(request, pk, format=None):
-    """ Retrieve, update or delete a host """
-    try:
-        host = Hosts.objects.get(pk=pk)
-    except Hosts.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class HostDetail(APIView):
+    """
+    Retrieve, update or delete a host instance.
+    """
 
-    if request.method == 'GET':
+    def get_object(self, pk):
+        try:
+            return Hosts.objects.get(pk=pk)
+        except Hosts.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        host = self.get_object(pk)
         serializer = HostsSerializer(host)
         return Response(serializer.data)
-    elif request.method == 'PUT':
-        # data = JSONParser().parse(request)
+
+    def put(self, request, pk, format=None):
+        host = self.get_object(pk)
         serializer = HostsSerializer(host, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, pk, format=None):
+        host = self.get_object(pk)
         host.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
--- mregsite/urls.py
+++ mregsite/urls.py
@@ -1,18 +1,3 @@
-"""mreg URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
@@ -20,8 +5,8 @@ from mreg.api.v1 import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    url(r'^hosts/$', views.host_list),
-    url(r'^hosts/(?P<pk>[0-9]+)/$', views.host_detail),
+    url(r'^hosts/$', views.HostList.as_view()),
+    url(r'^hosts/(?P<pk>[0-9]+)/$', views.HostDetail.as_view()),
     path('admin/', admin.site.urls),
 ]
 
