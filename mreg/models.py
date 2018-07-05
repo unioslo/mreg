b2908d953160c30701bd1b96f92ea04526833f50
Geir Ulvik <ulvik@usit.uio.no>
2018-07-05 14:53:43 +0200

    Revert models and validators to df99b134c7201beb8abf1d55e568c3222ac0734e (pre-signals implementation)

--- mreg/models.py
+++ mreg/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from mreg.validators import *
+from django.core.exceptions import ValidationError
 
 
 class Zones(models.Model):
@@ -7,26 +8,35 @@ class Zones(models.Model):
     name = models.TextField(unique=True)
     primary_ns = models.TextField()
     email = models.EmailField(blank=True, null=True)
-    serialno = models.BigIntegerField(blank=True, null=True)
+    serialno = models.BigIntegerField(blank=True, null=True, validators=[validate_zones_serialno])
     refresh = models.IntegerField(blank=True, null=True)
     retry = models.IntegerField(blank=True, null=True)
     expire = models.IntegerField(blank=True, null=True)
-    ttl = models.IntegerField(blank=True, null=True)
+    ttl = models.IntegerField(blank=True, null=True, validators=[validate_ttl])
 
     class Meta:
         db_table = 'zones'
 
+    def clean(self):
+        # TODO: Implement as validation with signal
+        # Make sure refresh, retry, and expire values adhere to database constraints
+        check_refresh = self.refresh > self.retry
+        check_expire = self.expire > self.refresh + self.retry
+        check_retry = self.retry >= 300
 
-# TODO: Find a way to move validations via signals to some local_validations?
-models.signals.pre_save.connect(validate_zones_refresh_retry_expire, sender=Zones)
-models.signals.pre_save.connect(validate_zones_serialno, sender=Zones)
+        if not check_refresh:
+            raise ValidationError('Refresh may not be less than or equal to retry.')
+        if not check_expire:
+            raise ValidationError('Expire must be greater than retry + refresh ({}).'.format(self.refresh+self.retry))
+        if not check_retry:
+            raise ValidationError('Retry may not be less than 300.')
 
 
 class Ns(models.Model):
     nsid = models.AutoField(primary_key=True, serialize=True)
     zoneid = models.ForeignKey('Zones', models.DO_NOTHING, db_column='zoneid')
     name = models.TextField()
-    ttl = models.IntegerField(blank=True, null=True)
+    ttl = models.IntegerField(blank=True, null=True, validators=[validate_ttl])
 
     class Meta:
         db_table = 'ns'
@@ -45,28 +55,22 @@ class Hosts(models.Model):
     hostid = models.AutoField(primary_key=True, serialize=True)
     name = models.TextField(unique=True)
     contact = models.EmailField()
-    ttl = models.IntegerField(blank=True, null=True)
+    ttl = models.IntegerField(blank=True, null=True, validators=[validate_ttl])
     hinfo = models.ForeignKey(HinfoPresets, models.DO_NOTHING, db_column='hinfo', blank=True, null=True)
-    loc = models.TextField(blank=True, null=True)
+    loc = models.TextField(blank=True, null=True, validators=[validate_loc])
     comment = models.TextField(blank=True, null=True)
 
     class Meta:
         db_table = 'hosts'
 
 
-#models.signals.pre_save.connect(validate_loc, sender=Hosts)
-
-
 class Ipaddress(models.Model):
     hostid = models.ForeignKey(Hosts, models.DO_NOTHING, db_column='hostid')
     ipaddress = models.GenericIPAddressField(unique=True)
-    macaddress = models.TextField(blank=True, null=True)
+    macaddress = models.TextField(blank=True, null=True, validators=[validate_mac_address])
 
     class Meta:
         db_table = 'ipaddress'
-
-
-models.signals.pre_save.connect(validate_mac_address, sender=Ipaddress)
 
 
 class PtrOverride(models.Model):
@@ -89,7 +93,7 @@ class Txt(models.Model):
 class Cname(models.Model):
     hostid = models.ForeignKey('Hosts', models.DO_NOTHING, db_column='hostid')
     cname = models.TextField()
-    ttl = models.IntegerField(blank=True, null=True)
+    ttl = models.IntegerField(blank=True, null=True, validators=[validate_ttl])
 
     class Meta:
         db_table = 'cname'
@@ -111,7 +115,7 @@ class Naptr(models.Model):
     hostid = models.ForeignKey('Hosts', models.DO_NOTHING, db_column='hostid')
     preference = models.IntegerField(blank=True, null=True)
     orderv = models.IntegerField(blank=True, null=True)
-    flag = models.CharField(max_length=1, blank=True, null=True)
+    flag = models.CharField(max_length=1, blank=True, null=True, validators=[validate_naptr_flag])
     service = models.TextField()
     regex = models.TextField(blank=True, null=True)
     replacement = models.TextField()
@@ -120,20 +124,14 @@ class Naptr(models.Model):
         db_table = 'naptr'
 
 
-models.signals.pre_save.connect(validate_naptr_flag, sender=Naptr)
-
-
 class Srv(models.Model):
     srvid = models.AutoField(primary_key=True, serialize=True)
-    service = models.TextField()
+    service = models.TextField(validators=[validate_srv_service_text])
     priority = models.IntegerField(blank=True, null=True)
     weight = models.IntegerField(blank=True, null=True)
     port = models.IntegerField(blank=True, null=True)
-    ttl = models.IntegerField(blank=True, null=True)
+    ttl = models.IntegerField(blank=True, null=True, validators=[validate_ttl])
     target = models.TextField(blank=True, null=True)
 
     class Meta:
         db_table = 'srv'
-
-
-models.signals.pre_save.connect(validate_srv_service_text, sender=Srv)
--- mreg/validators.py
+++ mreg/validators.py
@@ -1,64 +1,44 @@
 from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
-from django.core.exceptions import ValidationError
 
 
-def validate_ttl(sender, instance, **kwargs):
+# TODO: Rewrite validators to use signals
+def validate_ttl(value):
     """Validates that the ttl value is greater than or equal to a certain value."""
-    # TODO: ttl validation should probably happen in the client, not API.
-    ttl_limit = 300
-    if instance.ttl < ttl_limit:
-        raise ValidationError(
-            "ttl value must be greater than or equal to {} ({} given).".format(
-                ttl_limit, instance.ttl)
-        )
+    validator = MinValueValidator(0)
+    validator(value)
 
 
-def validate_mac_address(sender, instance, **kwargs):
+def validate_mac_address(address):
     """Validates that the mac address is on a valid form."""
     adr_regex = "[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$"
     validator = RegexValidator(adr_regex)
-    validator(instance.macaddress)
+    validator(address)
 
 
-def validate_loc(sender, instance, **kwargs):
+def validate_loc(location):
     """Validates that the loc input is on a valid form."""
     loc_regex = "\d+ \d+ \d+ [NS] \d+ \d+ \d+ [EW] \d+m"
     validator = RegexValidator(loc_regex)
-    validator(instance.loc)
+    validator(location)
 
 
-def validate_naptr_flag(sender, instance, **kwargs):
+def validate_naptr_flag(flag):
     """Validates that the naptr model flag input is valid."""
     flag_regex = "^[sAUP]$"
     validator = RegexValidator(flag_regex)
-    validator(instance.flag)
+    validator(flag)
 
 
-def validate_srv_service_text(sender, instance, **kwargs):
+def validate_srv_service_text(servicetext):
     """Validates that the srv service text input is valid."""
     servicetext_regex = '^_[a-z]+\._(tcp|udp)\.([\w\-]+\.)+$'
     validator = RegexValidator(servicetext_regex)
-    validator(instance.servicetext)
+    validator(servicetext)
 
 
-def validate_zones_serialno(sender, instance, **kwargs):
+def validate_zones_serialno(serialno):
     """ Validates that the zones serialno is within given parameters."""
     validator_min = MinValueValidator(1000000000)
     validator_max = MaxValueValidator(9999999999)
-    validator_min(instance.serialno)
-    validator_max(instance.serialno)
-
-
-def validate_zones_refresh_retry_expire(sender, instance, **kwargs):
-    """Validates that the refresh, retry, and expire values adhere to constraints."""
-    # TODO: Should probaly be moved to client.
-    check_refresh = instance.refresh > instance.retry
-    check_expire = instance.expire > instance.refresh + instance.retry
-    check_retry = instance.retry >= 300
-
-    if not check_refresh:
-        raise ValidationError('Refresh may not be less than or equal to retry.')
-    if not check_expire:
-        raise ValidationError('Expire must be greater than retry + refresh ({}).'.format(self.refresh + self.retry))
-    if not check_retry:
-        raise ValidationError('Retry may not be less than 300.')
+    validator_min(serialno)
+    validator_max(serialno)
