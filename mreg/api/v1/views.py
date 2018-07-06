fc5eaac99ad56056f22d4b291ecea64fb6cec626
Nils Hiorth <nils@nls.me>
2018-07-06 14:27:53 +0200

    Ipaddress is now optional when posting a new host

--- mreg/api/v1/views.py
+++ mreg/api/v1/views.py
@@ -39,20 +39,28 @@ class HostList(generics.GenericAPIView):
         return Response(serializer.data)
 
     def post(self, request):
-        ipaddress = request.data['ipaddress']
-        hostdata = QueryDict.copy(request.data)
-        del hostdata['ipaddress']
-        host = Hosts()
-        hostserializer = HostsSerializer(host, data=hostdata)
-        if hostserializer.is_valid(raise_exception=True):
-            hostserializer.save()
-            location = '/hosts/' + host.name
-            ipdata = {'hostid': host.pk, 'ipaddress': ipaddress}
-            ip = Ipaddress()
-            ipserializer = IpaddressSerializer(ip, data=ipdata)
-            if ipserializer.is_valid(raise_exception=True):
-                ipserializer.save()
-            return Response(hostserializer.data, status=status.HTTP_201_CREATED, headers={'Location': location})
+        if 'ipaddress' in request.data:
+            ipaddress = request.data['ipaddress']
+            hostdata = QueryDict.copy(request.data)
+            del hostdata['ipaddress']
+            host = Hosts()
+            hostserializer = HostsSerializer(host, data=hostdata)
+            if hostserializer.is_valid(raise_exception=True):
+                hostserializer.save()
+                location = '/hosts/' + host.name
+                ipdata = {'hostid': host.pk, 'ipaddress': ipaddress}
+                ip = Ipaddress()
+                ipserializer = IpaddressSerializer(ip, data=ipdata)
+                if ipserializer.is_valid(raise_exception=True):
+                    ipserializer.save()
+                return Response(hostserializer.data, status=status.HTTP_201_CREATED, headers={'Location': location})
+        else:
+            host = Hosts()
+            hostserializer = HostsSerializer(host, data=request.data)
+            if hostserializer.is_valid(raise_exception=True):
+                hostserializer.save()
+                location = '/hosts/' + host.name
+                return Response(hostserializer.data, status=status.HTTP_201_CREATED, headers={'Location': location})
 
 
 class HostDetail(ETAGMixin, generics.RetrieveUpdateDestroyAPIView):
