ef39d2a7c6aab5cdffee37ba7d5b94b9633f3be7
Geir Tore Ulvik <ulvik@vepsebol.uio.no>
2018-07-10 10:48:00 +0200

    Changed URL to match new structure for URLS with plurals etc.

--- mregsite/urls.py
+++ mregsite/urls.py
@@ -3,27 +3,32 @@ from django.urls import path
 from mreg.api.v1 import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
+# Ex: Making a new IP address is a POST request to /ipaddresses
+#     Changing an existing IP address is a PATCH request to /ipaddresses/<ip>/
+#
+# Equivalent for other fields.
+
 urlpatterns = [
-    path('cname/', views.CnameList.as_view()),
-    path('cname/<pk>/', views.CnameDetail.as_view()),
+    path('cnames/', views.CnameList.as_view()),
+    path('cnames/<pk>/', views.CnameDetail.as_view()),
     path('hinfopresets/', views.HinfoPresetsList.as_view()),
     path('hinfopresets/<pk>/', views.HinfoPresetsDetail.as_view()),
     path('hosts/', views.HostList.as_view()),
     path('hosts/<pk>/', views.HostDetail.as_view()),
-    path('ipaddress/', views.IpaddressList.as_view()),
-    path('ipaddress/<pk>/', views.IpaddressDetail.as_view()),
-    path('naptr/', views.NaptrList.as_view()),
-    path('naptr/<pk>/', views.NaptrDetail.as_view()),
-    path('ns/', views.NsList.as_view()),
-    path('ns/<pk>/', views.NsDetail.as_view()),
-    path('ptroverride/', views.PtrOverrideList.as_view()),
-    path('ptroverride/<pk>/', views.PtrOverrideDetail.as_view()),
-    path('srv/', views.SrvList.as_view()),
-    path('srv/<pk>/', views.SrvDetail.as_view()),
+    path('ipaddresses/', views.IpaddressList.as_view()),
+    path('ipaddresses/<pk>/', views.IpaddressDetail.as_view()),
+    path('naptrs/', views.NaptrList.as_view()),
+    path('naptrs/<pk>/', views.NaptrDetail.as_view()),
+    path('nameservers/', views.NsList.as_view()),
+    path('nameservers/<pk>/', views.NsDetail.as_view()),
+    path('ptroverrides/', views.PtrOverrideList.as_view()),
+    path('ptroverrides/<pk>/', views.PtrOverrideDetail.as_view()),
+    path('srvs/', views.SrvList.as_view()),
+    path('srvs/<pk>/', views.SrvDetail.as_view()),
     path('subnets/', views.SubnetsList.as_view()),
     path('subnets/<pk>/', views.SubnetsDetail.as_view()),
-    path('txt/', views.TxtList.as_view()),
-    path('txt/<pk>/', views.TxtDetail.as_view()),
+    path('txts/', views.TxtList.as_view()),
+    path('txts/<pk>/', views.TxtDetail.as_view()),
     path('zones/', views.ZonesList.as_view()),
     path('zones/<pk>/', views.ZonesDetail.as_view()),
     path('admin/', admin.site.urls),
