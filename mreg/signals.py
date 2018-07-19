0cc2543b1defce7a9d613d157aa332bddfd26f99
Geir Tore Ulvik <ulvik@vepsebol.uio.no>
2018-07-19 16:04:57 +0200

    Added logsaving to signal for hosts.

--- mreg/signals.py
+++ mreg/signals.py
@@ -1,9 +1,19 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from datetime import datetime
 
 from mreg.models import *
+from mreg.api.v1.serializers import *
 
 
 @receiver(post_save, sender=Hosts)
 def save_host_history(sender, instance, created, **kwargs):
-    print(instance.name)
+    content = HostsSerializer(instance).data
+    new_log_entry = ModelChangeLogs(table_name=Hosts,
+                                    table_row=content['hostid'],
+                                    data=content,
+                                    action='saved',
+                                    timestamp=datetime.now())
+    new_log_entry.save()
+    logs = ModelChangeLogs.objects.filter(table_row=content['hostid'])
+    print(log.data for log in logs)
