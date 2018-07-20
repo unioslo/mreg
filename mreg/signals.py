d04a36ecf6f34e24a1f59219e940cbe7f3d7d98e
Geir Tore Ulvik <ulvik@vepsebol.uio.no>
2018-07-20 13:47:58 +0200

    Changing signals to trigger when ForeignKeys are saved for host history.

--- mreg/signals.py
+++ mreg/signals.py
@@ -1,19 +1,24 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from datetime import datetime
+from django.utils import timezone
 
 from mreg.models import *
 from mreg.api.v1.serializers import *
 
 
-@receiver(post_save, sender=Hosts)
-def save_host_history(sender, instance, created, **kwargs):
-    content = HostsSerializer(instance).data
-    new_log_entry = ModelChangeLogs(table_name=Hosts,
-                                    table_row=content['hostid'],
-                                    data=content,
-                                    action='saved',
-                                    timestamp=datetime.now())
-    new_log_entry.save()
-    logs = ModelChangeLogs.objects.filter(table_row=content['hostid'])
-    print(log.data for log in logs)
+@receiver(post_save, sender=Ipaddress)
+def save_host_history_ip(sender, instance, created, **kwargs):
+
+    ip = IpaddressSerializer(instance)
+    hostdata = HostsSerializer(Hosts.objects.get(hostid=ip.data['hostid'])).data
+    hostdata['ipaddress'] = [arecord['ipaddress'] for arecord in hostdata['ipaddress']]
+    print(hostdata)
+
+    # new_log_entry = ModelChangeLogs(table_name=Hosts,
+    #                                 table_row=content['hostid'],
+    #                                 data=content,
+    #                                 action='saved',
+    #                                 timestamp=timezone.now())
+    # new_log_entry.save()
+    # logs = ModelChangeLogs.objects.filter(table_row=content['hostid']).order_by('timestamp').values('data')
+
