106bac4e8df88a61363c97b39f1fe0dd244e36e8
Øyvind Kolbu <oyvind.kolbu@usit.uio.no>
2018-10-25 09:11:16 +0200

    Preliminary zonefiles for reversezones.
    Still lacks ptroverride, and making sure all hostnames end with
    a punctation mark.

--- mreg/api/v1/zonefile.py
+++ mreg/api/v1/zonefile.py
@@ -1,18 +1,18 @@
-from mreg.models import Host, Naptr, Srv, PtrOverride
+from mreg.models import Host, Ipaddress, Naptr, Srv, PtrOverride
 import ipaddress
 
 class ZoneFile(object):
     def __init__(self, zone):
         self.zone = zone
         if zone.name.endswith('.in-addr.arpa'):
-            self.file = IPv4ReverseFile()
+            self.zonetype = IPv4ReverseFile()
         elif zone.name.endswith('.ip6.arpa'):
-            self.file = IPv6ReverseFile()
+            self.zonetype = IPv6ReverseFile()
         else:
-            self.file = ForwardFile()
+            self.zonetype = ForwardFile()
 
     def generate(self):
-        return self.file.generate(self.zone)
+        return self.zonetype.generate(self.zone)
 
 class ForwardFile(object):
     def generate(self, zone):
@@ -20,7 +20,7 @@ class ForwardFile(object):
         data = zone.zf_string
         data += ';\n; Name servers\n;\n'
         for ns in zone.nameservers.all():
-         data += ns.zf_string(zone.name)
+            data += ns.zf_string(zone.name)
         # Print info about hosts and their corresponding data
         data += ';\n; Host addresses\n;\n'
         hosts = Host.objects.filter(zoneid=zone.zoneid).order_by('name')
@@ -51,7 +51,86 @@ class ForwardFile(object):
         return data
 
 class IPv4ReverseFile(object):
-    pass
+
+    def get_network(self, zone):
+        zone = zone.replace('.in-addr.arpa','')
+        splitted = list(reversed(zone.split(".")))
+        netmask = 8 * len(splitted)
+        while len(splitted) < 4:
+            splitted.append("0")
+        net = ".".join(splitted)
+        return ipaddress.ip_network("{}/{}".format(net, netmask))
+
+    def get_ipaddresses(self, network):
+        from_ip = str(network.network_address)
+        to_ip = str(network.broadcast_address)
+        where_str = "ipaddress BETWEEN '{}' AND '{}'".format(from_ip, to_ip)
+        ips = Ipaddress.objects.extra(where=[where_str],
+                                       order_by=["ipaddress"])
+        # XXX: need to check ptroverrides
+        return ips
+
+    def __ip2origin(self, ip):
+        tmp = ip.split('.')[:3]
+        tmp.reverse()
+        return '$ORIGIN %s.in-addr.arpa.\n' % ".".join(tmp)
+
+    def generate(self, zone):
+        network = self.get_network(zone.name)
+        data = zone.zf_string
+        data += ';\n; Name servers\n;\n'
+        for ns in zone.nameservers.all():
+            data += ns.zf_string(zone.name)
+        # TODO: delegated entries, if any
+        origin = ''
+        this_net = 'z'
+        for ip in self.get_ipaddresses(network):
+            ptrip = ip.ipaddress
+            # XXX: simpler as v6?
+            if not ptrip.startswith(this_net):
+                this_net = ptrip[:ptrip.rfind(".")+1]
+                new_origin = self.__ip2origin(ptrip)
+                if origin != new_origin:
+                    data += new_origin
+                    origin = new_origin
+            ptrip = ptrip[ptrip.rfind(".")+1:]
+            data += "{}\tPTR\t{}\n".format(ptrip, ip.hostid.name)
+        return data
 
 class IPv6ReverseFile(object):
-    pass
+
+    def get_network(self, zone):
+        zone = zone.replace('.ip6.arpa','')
+        splitted = zone.split(".")
+        netmask = 4 * len(splitted)
+        net = ""
+        it = reversed(splitted)
+        for i in it:
+            net += "%s%s%s%s:" % (i, next(it, '0'), next(it, '0'), next(it, '0'))
+        return ipaddress.ip_network("{}:/{}".format(net, netmask))
+
+    def get_ipaddresses(self, network):
+        from_ip = str(network.network_address)
+        to_ip = str(network.broadcast_address)
+        where_str = "ipaddress BETWEEN '{}' AND '{}'".format(from_ip, to_ip)
+        ips = Ipaddress.objects.extra(where=[where_str],
+                                       order_by=["ipaddress"])
+        # XXX: need to check ptroverrides
+        return ips
+
+    def generate(self, zone):
+        network = self.get_network(zone.name)
+        data = zone.zf_string
+        data += ';\n; Name servers\n;\n'
+        for ns in zone.nameservers.all():
+            data += ns.zf_string(zone.name)
+        # TODO: delegated entries, if any
+        _prev_net = 'z'
+        for ip in self.get_ipaddresses(network):
+            rev = ipaddress.ip_address(ip.ipaddress).reverse_pointer
+            # Add $ORIGIN between every new /64 found
+            if not rev.endswith(_prev_net):
+                _prev_net = rev[32:]
+                data += "$ORIGIN {}.\n".format(_prev_net)
+            data += "{}\tPTR\t{}\n".format(rev[:31], ip.hostid.name)
+        return data
