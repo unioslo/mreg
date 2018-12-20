100cd3e5a3ab9282705e18b07be71a9efb40ffa1
Øyvind Kolbu <oyvind.kolbu@usit.uio.no>
2018-12-20 11:52:21 +0100

    Adapt to new result from zone.get_ipaddresses().

--- mreg/api/v1/zonefile.py
+++ mreg/api/v1/zonefile.py
@@ -82,14 +82,14 @@ class IPv4ReverseFile(object):
         # TODO: delegated entries, if any
         data += ';\n; Delegations \n;\n'
         _prev_net = 'z'
-        for ip in zone.get_ipaddresses():
-            rev = ipaddress.ip_address(ip.ipaddress).reverse_pointer
+        for ip, hostname in zone.get_ipaddresses():
+            rev = ip.reverse_pointer
             # Add $ORIGIN between every new /24 found
             if not rev.endswith(_prev_net):
                 _prev_net = rev[rev.find('.'):]
                 data += "$ORIGIN {}.\n".format(_prev_net[1::])
             ptrip = rev[:rev.find('.')]
-            data += "{}\tPTR\t{}.\n".format(ptrip, idna_encode(ip.host.name))
+            data += "{}\tPTR\t{}.\n".format(ptrip, idna_encode(hostname))
         return data
 
 
@@ -103,11 +103,11 @@ class IPv6ReverseFile(object):
         # TODO: delegated entries, if any
         data += ';\n; Delegations\n;\n'
         _prev_net = 'z'
-        for ip in zone.get_ipaddresses():
-            rev = ipaddress.ip_address(ip.ipaddress).reverse_pointer
+        for ip, hostname in zone.get_ipaddresses():
+            rev = ip.reverse_pointer
             # Add $ORIGIN between every new /64 found
             if not rev.endswith(_prev_net):
                 _prev_net = rev[32:]
                 data += "$ORIGIN {}.\n".format(_prev_net)
-            data += "{}\tPTR\t{}.\n".format(rev[:31], idna_encode(ip.host.name))
+            data += "{}\tPTR\t{}.\n".format(rev[:31], idna_encode(hostname))
         return data
