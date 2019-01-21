93f48b089e758d147403da81db2d2166c3a2a2a6
Øyvind Kolbu <oyvind.kolbu@usit.uio.no>
2019-01-21 15:38:13 +0100

    Add API to obtain token, and let the tokens expire after 8 hours.
    From https://stackoverflow.com/questions/14567586/token-authentication-for-restful-api-should-the-token-be-periodically-changed

---
+++ mreg/authentication.py
@@ -0,0 +1,24 @@
+from datetime import timedelta
+from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils import timezone
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import exceptions
+
+EXPIRE_HOURS = getattr(settings, 'REST_FRAMEWORK_TOKEN_EXPIRE_HOURS', 8)
+
+
+class ExpiringTokenAuthentication(TokenAuthentication):
+    def authenticate_credentials(self, key):
+        try:
+            token = self.get_model().objects.get(key=key)
+        except ObjectDoesNotExist:
+            raise exceptions.AuthenticationFailed('Invalid token')
+
+        if not token.user.is_active:
+            raise exceptions.AuthenticationFailed('User inactive or deleted')
+
+        if token.created < timezone.now() - timedelta(hours=EXPIRE_HOURS):
+            raise exceptions.AuthenticationFailed('Token has expired')
+
+        return token.user, token
