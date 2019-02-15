a6bc95bf2cb85025925f588b0e4d453199bd685c
Øyvind Kolbu <oyvind.kolbu@usit.uio.no>
2019-02-15 14:14:57 +0100

    Gunicorn did not work.. seems like someone renamed a directory.

--- mregsite/wsgi.py
+++ mregsite/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mreg.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mregsite.settings")
 
 application = get_wsgi_application()
