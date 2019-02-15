727d3ee4edc219adf8bada4a02b156b994e0bdf4
Øyvind Kolbu <oyvind.kolbu@usit.uio.no>
2019-02-15 14:19:49 +0100

    Remove outdated comment.

--- mregsite/urls.py
+++ mregsite/urls.py
@@ -7,7 +7,7 @@ from rest_framework_swagger.views import get_swagger_view
 # Schema view for swagger api documentation
 schema_view = get_swagger_view(title='mreg API')
 
-# The resource keyword argument is used by StrictCRUDMixin to determine the base url for generic views
+#
 urlpatterns = [
     path('', include('mreg.api.v1.urls')),
     path('api/', include('mreg.api.urls')),
