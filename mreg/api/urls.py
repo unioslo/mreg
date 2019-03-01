a17fa6b214cdf6878e64a383fce6ea78c30c58d2
Øyvind Kolbu <oyvind.kolbu@usit.uio.no>
2019-03-01 15:40:23 +0100

    Add API to logout a token. Actually just delete it.

--- mreg/api/urls.py
+++ mreg/api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('logout/', views.Logout.as_view()),
     path('token-auth/', views.ObtainExpiringAuthToken.as_view()),
 ]
