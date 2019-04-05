da080b61189aaf28f9b2a1f7b0fac96ef45b87d2
Øyvind Kolbu <oyvind.kolbu@usit.uio.no>
2019-04-05 08:36:12 +0200

    Add a new user model which shall be the new default.

---
+++ mreg/models_auth.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+
+    _group_list = None
+
+    @property
+    def group_list(self):
+        if self._group_list is None:
+            self._group_list = list(self.groups.values_list('name', flat=True))
+        return self._group_list
