93c1acddf800bdf20577ade6522c80eee1958862
Øyvind Kolbu <oyvind.kolbu@usit.uio.no>
2019-05-15 12:19:43 +0200

    Add two new fields. LCICharField and DnsNameField.
    LCICharField is a case insensitive char field which stores all values
    in lower case.
    
    DnsNameField is a convenience field with max_length and validator set.

---
+++ mreg/fields.py
@@ -0,0 +1,24 @@
+import django.contrib.postgres.fields as pgfields
+
+from .validators import validate_hostname
+
+class LCICharField(pgfields.CICharField):
+    """A pgfields.CICharField where the value is stored in lower case.  """
+
+    def get_db_prep_save(self, value, connection):
+        if isinstance(value, str):
+            value = value.lower()
+        return super().get_db_prep_save(value, connection)
+
+
+class DnsNameField(LCICharField):
+    """
+    A field to hold DNS names.
+
+    It is stored all lower case and matching is done case insensitive.
+    """
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 253
+        if 'validators' not in kwargs:
+            kwargs['validators'] = [validate_hostname]
+        super().__init__(*args, **kwargs)
