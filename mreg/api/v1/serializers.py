unioslo/mreg

commit 5b810831435a3b332d166b4ac187e82007a09421

Add support for MAC addresses in different formats.
* mreg/validators.py (validate_normalizeable_mac_address): New function.
* mreg/utils.py (normalize_mac): New function.
* mreg/api/v1/serializers.py (MacAddressSerializerField): New SerializerField.
(IpaddressSerializer): Declare it.
* mreg/api/v1/tests/tests.py (APIMACaddressTestCase.test_mac_patch_formats_204_ok):
New test.
(APIMACaddressTestCase.test_mac_patch_invalid_mac_400_bad_request):
Adjust uppercase MAC test to use invalid separators because uppercase
MAC is now allowed.

mreg/api/v1/serializers.py
@@ -11,8 +11,8 @@ from mreg.models import (Cname, ForwardZone, ForwardZoneDelegation,
                          Mx, NameServer, Naptr,
                          NetGroupRegexPermission, Network, PtrOverride,
                          ReverseZone, ReverseZoneDelegation, Srv, Sshfp, Txt)
-from mreg.utils import nonify
-from mreg.validators import validate_keys
+from mreg.utils import (nonify, normalize_mac)
+from mreg.validators import (validate_keys, validate_normalizeable_mac_address)
 
 
 class ValidationMixin:
@@ -64,8 +64,20 @@ class HinfoSerializer(ValidationMixin, serializers.ModelSerializer):
         model = Hinfo
         fields = '__all__'
 
+class MacAddressSerializerField(serializers.Field):
+    """Normalize the provided MAC address into the common format."""
+    def to_representation(self, obj):
+        return obj
+
+    def to_internal_value(self, data):
+        if data and isinstance(data, str):
+            validate_normalizeable_mac_address(data)
+            return normalize_mac(data)
+        return data
 
 class IpaddressSerializer(ValidationMixin, serializers.ModelSerializer):
+    macaddress = MacAddressSerializerField(required=False)
+
     class Meta:
         model = Ipaddress
         fields = '__all__'

mreg/api/v1/tests/tests.py
@@ -1173,6 +1173,16 @@ class APIMACaddressTestCase(MregAPITestCase):
         self.assert_patch_and_400('/ipaddresses/%s' % self.ipaddress_one.id,
                                   patch_mac_in_use)
 
+    def test_mac_patch_formats_204_ok(self):
+        """ Patch an IP with MAC address in various formats. """
+        def _assert(mac):
+            self.assert_patch('/ipaddresses/%s' % self.ipaddress_one.id,
+                              {'macaddress': mac})
+        _assert('AA:BB:CC:00:11:22')
+        _assert('AA-BB-CC-00-11-22')
+        _assert('aabb.cc00.1122')
+        _assert('aa:bb:cc:00:11:22')
+
     def test_mac_patch_invalid_mac_400_bad_request(self):
         """ Patch an IP with invalid MAC should return 400 bad request."""
         def _assert(mac):
@@ -1180,7 +1190,7 @@ class APIMACaddressTestCase(MregAPITestCase):
                                       {'macaddress': mac})
         _assert('00:00:00:00:00:XX')
         _assert('00:00:00:00:00')
-        _assert('AA:BB:cc:dd:ee:ff')
+        _assert('aa_bb_cc_dd_ee_ff')
 
     def test_mac_patch_to_ip_to_network_with_mac_in_use(self):
         """Test that it is not allowed to patch an Ipaddress with a new ipaddress

mreg/utils.py
@@ -119,3 +119,14 @@ def get_network_from_zonename(name):
         for i in it:
             net += "%s%s%s%s:" % (i, next(it, '0'), next(it, '0'), next(it, '0'))
         return ipaddress.ip_network("{}:/{}".format(net, netmask))
+
+# Taken from mreg_cli.util.format_mac.
+def normalize_mac(mac: str) -> str:
+    """
+    Create a strict 'aa:bb:cc:11:22:33' MAC address.
+    Replaces any other delimiters with a colon and turns it into all lower
+    case.
+    """
+    mac = re.sub('[.:-]', '', mac).lower()
+    return ":".join(["%s" % (mac[i:i+2]) for i in range(0, 12, 2)])
+

mreg/validators.py
@@ -122,6 +122,12 @@ def validate_mac_address(address):
                                message="Must be on form: aa:bb:cc:00:11:22")
     validator(address)
 
+def validate_normalizeable_mac_address(address):
+    """Validates that the mac address can be normalized."""
+    adr_regex = "^([a-fA-F0-9]{2}[:.-]?){5}[a-fA-F0-9]{2}$"
+    validator = RegexValidator(adr_regex,
+                               message="Invalid MAC address.")
+    validator(address)
 
 def validate_loc(location):
     """Validates that the loc input is on a valid form."""

commit a3f1023c13db5eb747cc1395fdb4cc9cf8e9d251

api: Allow empty values for macaddress.
This addresses a regression introduced in
5b810831435a3b332d166b4ac187e82007a09421 where empty values for MAC
address would be silently ignored.

Fixes #421.

* mreg/api/v1/serializers.py (MacAddressSerializerField): Use
CharField as base class.
(IpaddressSerializer): Pass allow_blank=True for the macaddress serializer.
* mreg/api/v1/tests/tests.py (APIMACaddressTestCase.test_mac_remove_mac_204_ok):
Verify that emptying the MAC address actually works.

mreg/api/v1/serializers.py
@@ -65,7 +65,7 @@ class HinfoSerializer(ValidationMixin, serializers.ModelSerializer):
         model = Hinfo
         fields = '__all__'
 
-class MacAddressSerializerField(serializers.Field):
+class MacAddressSerializerField(serializers.CharField):
     """Normalize the provided MAC address into the common format."""
     def to_representation(self, obj):
         return obj
@@ -77,7 +77,7 @@ class MacAddressSerializerField(serializers.Field):
         return data
 
 class IpaddressSerializer(ValidationMixin, serializers.ModelSerializer):
-    macaddress = MacAddressSerializerField(required=False)
+    macaddress = MacAddressSerializerField(required=False, allow_blank=True)
 
     class Meta:
         model = Ipaddress

mreg/api/v1/tests/tests.py
@@ -1177,6 +1177,10 @@ class APIMACaddressTestCase(MregAPITestCase):
         """Patch an IP to remove MAC should return 204 ok."""
         self.assert_patch('/ipaddresses/%s' % self.ipaddress_one.id,
                           {'macaddress': ''})
+        # Assert that the MAC was actually removed, ref #421.
+        res = self.assert_get("/api/v1/hosts/?ipaddresses__ipaddress=%s"
+                              % self.ipaddress_one.ipaddress)
+        self.assertEqual(res.json()['results'][0]['ipaddresses'][0]['macaddress'], "")
 
     def test_mac_patch_mac_in_use_409_conflict(self):
         """Patch an IP with a MAC in use should return 409 conflict."""