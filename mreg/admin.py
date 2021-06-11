a3eb6feee20658cf5173d0c1aff8ffd0266fa932
Marius Bakke <marius.bakke@usit.uio.no>
2021-06-11 13:42:23 +0200

    Add admin view for ExpiringToken.
    * mreg/admin.py (AuthTokenAdmin): New class.

--- mreg/admin.py
+++ mreg/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import (User, ExpiringToken)
 
 admin.site.register(User, UserAdmin)
+
+@admin.register(ExpiringToken)
+class AuthTokenAdmin(admin.ModelAdmin):
+    list_display = ('key', 'user', 'created', 'last_used')
