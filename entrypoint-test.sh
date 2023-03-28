b73ed701133de25f65fc153cd61c164875ebb9b5
Øyvind Hagberg <oyvind.hagberg@usit.uio.no>
2023-03-28 12:05:07 +0200

    Bugfix: Entrypoint created citext ext in wrong db
    When using entrypoint-test.sh, the CITEXT extension ended up being
    created in the regular mreg database, when it needs to be created in the
    template1 database so it propagates to the temporary database created by
    manage.py test.

--- entrypoint-test.sh
+++ entrypoint-test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 cd /app
-./manage.py create_citext_extension
+./manage.py create_citext_extension --database template1
 ./manage.py test --noinput --failfast
--- mreg/management/commands/create_citext_extension.py
+++ mreg/management/commands/create_citext_extension.py
@@ -1,17 +1,38 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.db import connection
 from sys import stdout
+from django.conf import settings
+from psycopg2 import connect
 
 class Command(BaseCommand):
 	help = 'Create the CITEXT extension in the database.'
 
-	def handle(self, *args, **kwargs):
+	def add_arguments(self, parser):
+		# optional argument
+		parser.add_argument(
+			'--database',
+			type=str,
+			help='Database name',
+		)
+
+	def handle(self, *args, **options):
 		stdout.write("Attempting to create the CITEXT extension in the database...\n")
 		stdout.flush()
 		try:
-			with connection.cursor() as cursor:
+			con = connection
+			if options['database']:
+				stdout.write(f"Connecting to database {options['database']}\n")
+				stdout.flush()
+				con = connect(host=settings.DATABASES['default']['HOST'],
+					user=settings.DATABASES['default']['USER'],
+					password=settings.DATABASES['default']['PASSWORD'],
+					database=options['database'])
+			with con.cursor() as cursor:
 				cursor.execute("CREATE EXTENSION IF NOT EXISTS citext")
 				stdout.write(cursor.statusmessage+"\n")
 				stdout.flush()
+				con.commit()
 		except Exception as e:
+			stdout.write(e.__str__())
+			stdout.flush()
 			raise CommandError('Failed to create the CITEXT extension in the database.')
