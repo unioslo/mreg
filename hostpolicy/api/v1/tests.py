unioslo/mreg

commit 014e25b6030a05adf9622694539b92a38f71a2b1

How much do we trust the tests?
  - Code paths we should never reach aren't reached during tests...

mreg/api/v1/history.py
@@ -11,12 +11,7 @@ from mreg.models import History
 class DjangoJSONModelEncoder(DjangoJSONEncoder):
 
     def default(self, o):
-
-        if isinstance(o, Model):
-            return model_to_dict(o)
-
-        # TODO: #485 We should never ever hit this. Should we fail?
-        return super().default(o)  # pragma: no cover
+        return model_to_dict(o)
 
 
 class HistoryLog:

mreg/api/v1/serializers.py
@@ -79,12 +79,9 @@ class MacAddressSerializerField(serializers.CharField):
         return obj
 
     def to_internal_value(self, data):
-        if data and isinstance(data, str):
-            validate_normalizeable_mac_address(data)
-            return normalize_mac(data)
-        # We never get here. What other than str can the data be?
-        # TODO: Check data paths.
-        return data  # pragma: no cover
+        validate_normalizeable_mac_address(data)
+        return normalize_mac(data)
+
 
 class IpaddressSerializer(ValidationMixin, serializers.ModelSerializer):
     macaddress = MacAddressSerializerField(required=False, allow_blank=True)
@@ -245,11 +242,8 @@ class NetworkExcludedRangeSerializer(ValidationMixin, serializers.ModelSerialize
 
         def _get_ip(attr):
             ip = data.get(attr) or getattr(self.instance, attr)
-            # ip should be an object at this point, and it's never a string.
-            # TODO: Check data path and remove?
-            if isinstance(ip, str):  # pragma: no cover
-                return ipaddress.ip_address(ip)
-            return ip
+            return ipaddress.ip_address(ip)
+
         start_ip = _get_ip('start_ip')
         end_ip = _get_ip('end_ip')
         network_obj = data.get('network') or self.instance.network

commit f950f0a0e2aab0123577b261212ed9b8b5073bad

Fix a bug in history.py / DjangoJSONModelEncoder
The bug was introduced in 014e25b6030a05adf9622694539b92a38f71a2b1
when removing what was thought to be unused code paths.
Unfortunately, we didn't have a test that covered the particular case
where this code path was used.
This commit adds a test and fixes the bug.

hostpolicy/api/v1/tests.py
@@ -99,6 +99,10 @@ class HostPolicyAtomTestCase(HostPolicyRoleTestCase):
         self.object_one, _ = HostPolicyAtom.objects.get_or_create(name='testatom1')
         self.object_two, _ = HostPolicyAtom.objects.get_or_create(name='testatom2')
 
+    def test_create_with_date(self):
+        post_data = {"name": "orange", "description": "Round and orange", "create_date": "2018-07-07"}
+        self.assert_post('/api/v1/hostpolicy/atoms/', post_data)
+
 
 class HostPolicyAdminRights(MregAPITestCase):
     """Test that all of the API for HostPolicy are available for the admin group

mreg/api/v1/history.py
@@ -11,7 +11,9 @@ from mreg.models import History
 class DjangoJSONModelEncoder(DjangoJSONEncoder):
 
     def default(self, o):
-        return model_to_dict(o)
+        if isinstance(o, Model):
+            return model_to_dict(o)
+        return super().default(o)
 
 
 class HistoryLog: