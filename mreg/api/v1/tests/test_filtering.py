9b65eb1647fbe3316a3cdea554e2f6bd1b6c4eb7
Terje Kvernes <terje@kvernes.no>
2024-08-07 20:03:34 +0200

    Skeleton for testing filters.

---
+++ mreg/api/v1/tests/test_filtering.py
@@ -0,0 +1,67 @@
+
+from typing import List
+
+from mreg.models.host import Host
+from mreg.models.resource_records import Cname
+
+from .tests import MregAPITestCase
+
+from unittest_parametrize import param
+from unittest_parametrize import parametrize
+from unittest_parametrize import ParametrizedTestCase
+
+
+class FilterTestCase(ParametrizedTestCase, MregAPITestCase):
+    """Test filtering."""
+
+    # endpoint, query_key, target, expected_hits
+    #
+    # NOTE: The generated hostnames are UNIQUE across every test case!
+    # The format is: f"{endpoint}{query_key}{i}.example.com".replace("_", "")
+    # where i is the index of the hostname (and we make three for each test).
+    @parametrize(
+        ("endpoint", "query_key", "target", "expected_hits"),
+        [
+            param("hosts", "name", "hostsname0.example.com", 1, id="hosts_name"),
+            param("cnames", "host__name",  "cnameshostname1.example.com", 1, id="cnames_host__name"),
+            param("cnames", "host__name__icontains",  "cnameshostnameicontains", 3, id="cnames_host__icontains"),
+        ],
+
+    )
+    def test_filtering_for_host(self, endpoint: str, query_key: str, target: str, expected_hits: str) -> None:
+        """Test filtering on host."""
+
+        generate_count = 3
+        msg_prefix = f"{endpoint} : {query_key} -> {target} => "
+
+        hosts: List[Host] = []
+        cnames: List[Cname] = []
+        for i in range(generate_count):
+            hostname = f"{endpoint}{query_key}{i}.example.com".replace("_", "")
+            hosts.append(Host.objects.create(
+                name=hostname,
+                contact="admin@example.com",
+                ttl=3600,
+                comment="Test host",
+            ))
+        
+        for i in range(generate_count):
+            cname = f"cname.{endpoint}{query_key}{i}.example.com".replace("_", "")
+            cnames.append(Cname.objects.create(
+                host=hosts[i],
+                name=cname,
+                ttl=3600
+            ))
+
+        hostname = hosts[0].name
+        response = self.client.get(f"/api/v1/{endpoint}/?{query_key}={target}")
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(data["count"], expected_hits, msg=f"{msg_prefix} {data}")
+
+        for host in hosts:
+            host.delete()
+
+        for cname in cnames:
+            cname.delete()
+
