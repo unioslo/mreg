65c3219c58c9e996e78bc0752f8dc64e3ca361c1
Terje Kvernes <terje@kvernes.no>
2024-11-22 11:06:43 +0100

    Add user info endpoint.
    This endpoint shows some meta information about the user in question. Normal users can see their own information, superusers, admins, group admins, and network admins can add `?username=foo` to see information about that user.
    
    Information provided:
    
      - username
      - django status for the user (superuser, admin, active)
      - mreg status for the user (superuser, admin, group admin, network admin, hostpolicy admin, dns wildcard admin, and underscore admin).
      - group memberships
      - mreg permissions granted based on the groups one is a member of.

--- mreg/api/urls.py
+++ mreg/api/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('token-logout/', views.TokenLogout.as_view()),
     path('token-auth/', views.ObtainExpiringAuthToken.as_view()),
     path('token-is-valid/', views.TokenIsValid.as_view()),
+    path('meta/user', views.UserInfo.as_view()),
     path('meta/version', views.MregVersion.as_view()),
     path('meta/libraries', views.MetaVersions.as_view()),
     path('meta/heartbeat', views.MetaHeartbeat.as_view()),
--- mreg/api/views.py
+++ mreg/api/views.py
@@ -3,11 +3,12 @@ import time
 from typing import Any, cast
 
 import django
+from django.conf import settings
 import structlog
 
 from rest_framework import serializers, status
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.exceptions import AuthenticationFailed, PermissionDenied, NotFound
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -18,6 +19,8 @@ from psycopg2 import __libpq_version__ as libpq_version
 
 from mreg.api.permissions import IsSuperOrNetworkAdminMember
 from mreg.models.base import ExpiringToken
+from mreg.models.auth import User
+from mreg.models.network import NetGroupRegexPermission
 from mreg.__about__ import __version__ as mreg_version
 
 logger = structlog.getLogger(__name__)
@@ -92,6 +95,78 @@ class TokenIsValid(APIView):
     def get(self, request: Request):
         return Response(status=status.HTTP_200_OK)  
 
+###
+### User infomation views
+####
+
+class UserInfo(APIView):
+
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request: Request):
+        # Identify the requesting user
+        user = request.user
+        groups = user.groups.all()
+        is_mreg_superuser = settings.SUPERUSER_GROUP in [group.name for group in groups]
+        is_mreg_admin = settings.ADMINUSER_GROUP in [group.name for group in groups]
+        is_mreg_group_admin = settings.GROUPADMINUSER_GROUP in [group.name for group in groups]
+        is_mreg_network_admin = settings.NETWORK_ADMIN_GROUP in [group.name for group in groups]
+        is_mreg_hostpolicy_admin = settings.HOSTPOLICYADMIN_GROUP in [group.name for group in groups]
+        is_mreg_dns_wildcard_admin = settings.DNS_WILDCARD_GROUP in [group.name for group in groups]
+        is_mreg_dns_underscore_admin = settings.DNS_UNDERSCORE_GROUP in [group.name for group in groups]
+
+        # Determine target user (default is the requesting user)
+        username = request.query_params.get("username")
+        target_user = user
+
+        if username:
+            # Only allow access to other user data if the requester is an mreg superuser
+            if not is_mreg_superuser or is_mreg_admin or is_mreg_group_admin or is_mreg_network_admin:
+                raise PermissionDenied("You do not have permission to view other users' details.")
+            try:
+                target_user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                raise NotFound(f"User with username '{username}' not found.")
+
+        # Gather target user's information
+        target_groups = target_user.groups.all()
+        target_permissions = NetGroupRegexPermission.objects.filter(
+            group__in=[group.name for group in target_groups]
+        )
+
+        data = {
+            "username": target_user.username,
+            "django_status": {
+                "superuser": target_user.is_superuser,
+                "staff": target_user.is_staff,
+                "active": target_user.is_active,
+            },
+            "mreg_status": {
+                "superuser": is_mreg_superuser,
+                "admin": is_mreg_admin,
+                "group_admin": is_mreg_group_admin,
+                "network_admin": is_mreg_network_admin,
+                "hostpolicy_admin": is_mreg_hostpolicy_admin,
+                "dns_wildcard_admin": is_mreg_dns_wildcard_admin,
+                "underscore_admin": is_mreg_dns_underscore_admin
+            },
+            "groups": [group.name for group in target_groups],
+            "permissions": [
+                {
+                    "group": permission.group,
+                    "range": permission.range,
+                    "regex": permission.regex,
+                    "labels": [label.name for label in permission.labels.all()],
+                }
+                for permission in target_permissions
+            ],
+        }
+
+        return Response(status=status.HTTP_200_OK, data=data)
+    
+###
+### Introspection views
+###
 class MregVersion(APIView):
     
     permission_classes = (IsAuthenticated,)
