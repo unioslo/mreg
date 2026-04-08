94d5ccbc3435cfe2382202f33dc1002415f54056
pederhan <pederhan@usit.uio.no>
2026-04-08 13:20:19 +0200

    Rename URLs

--- mregsite/urls.py
+++ mregsite/urls.py
@@ -5,20 +5,18 @@ from django.conf import settings
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 
-swagger_view = SpectacularSwaggerView.as_view(url_name='schema')
 
 urlpatterns = [
     path('api/', include('mreg.api.urls')),
     path('api/v1/', include('hostpolicy.api.v1.urls')),
     path('api/v1/', include('mreg.api.v1.urls')),
     path('admin/', admin.site.urls),
-    # Download the schema as a file
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
     # Schema UIs
-    path('schema/swagger/', swagger_view, name='swagger'),
-    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    # Legacy alias for /docs/
-    path('docs/', swagger_view, name='docs'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
+    path('docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    # Download the schema as a file
+    path('docs/schema/', SpectacularAPIView.as_view(), name='schema'),
+
 ]
 
 if settings.MREG_PROFILING_ENABLED:
