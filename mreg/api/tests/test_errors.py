38c59a3b5839595c366a707129ce8f5d5106bf5d
pederhan <pederhan@usit.uio.no>
2026-09-22 12:04:47 +0200

    Refactor/expand ErrorCode string tests

--- mreg/api/tests/test_errors.py
+++ mreg/api/tests/test_errors.py
@@ -6,12 +6,17 @@ from mreg.api.errors import ErrorCode
 
 class ErrorCodeTestCase(SimpleTestCase):
     def test_error_code_in_f_strings(self):
+        """Test that the ErrorCode values behave correctly in f-strings."""
         # We need to make sure these behave consistently as strings and f-strings
         # since Python 3.11 brought breaking changes to f-string evaluation of enums.
         # See: https://blog.pecar.me/python-enum/
         message = f"Error code is {ErrorCode.REQUIRED}"
-        self.assertEqual(str(ErrorCode.REQUIRED), "required")
         self.assertEqual(message, "Error code is required")
+
+    def test_error_code_is_string(self):
+        """Test that the ErrorCode values behave as strings."""
+        self.assertEqual(ErrorCode.REQUIRED, "required")
+        self.assertEqual(str(ErrorCode.REQUIRED), "required")
 
 
     def test_composed_drf_error_codes(self):
